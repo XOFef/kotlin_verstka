36506cf82716e645664b24277ae96f4bddab3b22
KeQing <reterter213@yandex.ru>
2024-10-22 16:45:57 +0300

    Ошибка в файлах котлин

---
+++ app/src/main/java/com/example/keqing/Onboarding.kt
@@ -0,0 +1,23 @@
+package com.example.keqing
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Button
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Onboarding : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_onboarding)
+
+            val btn = findViewById<Button>(R.id.button_one)
+
+        }
+    }
---
+++ app/src/main/java/com/example/keqing/SplashScreen.kt
@@ -0,0 +1,25 @@
+package com.example.keqing
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.os.Handler
+import android.os.Looper
+import kotlinx.coroutines.delay
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed(
+            { startActivity(Intent(applicationContext, Onboarding::class.java))},
+        3000
+        )
+
+    }
+}
